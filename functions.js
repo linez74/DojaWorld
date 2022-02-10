2125fbc744b53dcb5cfc6c9d09db758df976d1d2
linez74 <83675084+linez74@users.noreply.github.com>
2022-02-10 14:17:44 +0200

    EVERYTHING AHAHAH

--- functions.js
+++ functions.js
@@ -68,3 +68,11 @@ function show(x){
 }
 
 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
